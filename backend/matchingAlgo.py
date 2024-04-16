94be4736631bd2d24dfee435c2e6b19a632733e3
Jonatan Salmeron <jonajere1@hotmail.com>
2024-04-16 01:20:00 -0400

    createPreferenceList() WIP, setSelectionPools() done for now

--- backend/matchingAlgo.py
+++ backend/matchingAlgo.py
@@ -14,32 +14,34 @@ firebase=pyrebase.initialize_app(firebaseConfig) #Initializes the firebase app
 
 db = firebase.database()
 
-freshmenBuildings = ['Delft House', 'Groningen House', 'Hague House', 'Leiden House', 'Rotterdam House', 'Tilburg House', 'Urtrecht House', 'Orange House', 'Rensselaer House',
-                     'Breukelen House', 'Amsterdam House', 'Stuyvesant Hall']
-
-#Orders all users into their appropriate selection pool
+#Orders all users into their appropriate selection pool.
 def setSelectionPools():
+    freshmenBuildings = ['Delft House', 'Groningen House', 'Hague House', 'Leiden House', 'Rotterdam House',    #These buildings only have 1 room type, so they only have 2 selection pools each
+                         'Tilburg House', 'Urtrecht House', 'Orange House', 'Rensselaer House',
+                     'Breukelen House', 'Amsterdam House', 'Stuyvesant Hall']
+    
     userList = db.child('users').get()
+    
     for student in userList:
         gender = student.val()['gender']
         pool = student.val()['buildingPreference'][0]
         
-        if ((pool in freshmenBuildings) == False):                                                                      #Not the Nethies
+        if ((pool in freshmenBuildings) == False):                                                                      #Not the freshmen buildings
             roomPreference = student.val()['roomType'] 
             studentArray = db.child('selectionPools').child(pool).child(gender).child(roomPreference).get().val()
             #print(studentArray)
 
-            if ((studentArray is None) or (studentArray[0] is None)):
+            if ((studentArray is None) or (studentArray[0] is None) or (studentArray[0] == "")):
                 studentArray = [student.key()]
             elif(student.key() not in studentArray):
                 studentArray.append(student.key())
 
             db.child('selectionPools').child(pool).child(gender).child(roomPreference).set(studentArray)
         
-        else:                                                                                                           #Nethies
+        else:                                                                                                           #freshmen buildings
             studentArray = db.child('selectionPools').child(pool).child(gender).get().val()
 
-            if ((studentArray is None) or (studentArray[0] is None)):
+            if ((studentArray is None) or (studentArray[0] is None) or (studentArray[0] == "")):
                 studentArray = [student.key()]
             elif(student.key() not in studentArray):
                 studentArray.append(student.key())
@@ -49,9 +51,39 @@ def setSelectionPools():
 
 #setSelectionPools()
 
-#TO DO: Test setSelectionPools() by resetting Alliance Hall data
-#       Implement the student preference list algorithm
+#TO DO: Test setSelectionPools() by resetting Alliance Hall data - CHECK
+#       Implement the student preference list algorithm - WIP
 #       Implement the final student matching algorithm
+#       Implement function to retrieve user's preference list as a sorted array of users in the same pool as them
+
+#Compares the questionnaire answers between each possible pair of users in a selection pool.
+def compareLists(selectionPool):
+    pass
+
+#Creates a preference list for every user in every selection pool.
+#The lists for each user is limited to the same selection pool the user is in.
+def createPreferenceLists():
+    freshmenBuildings = ['Delft House', 'Groningen House', 'Hague House', 'Leiden House', 'Rotterdam House', 'Tilburg House', 'Urtrecht House', 'Orange House', 'Rensselaer House',
+                     'Breukelen House', 'Amsterdam House', 'Stuyvesant Hall']
+    setSelectionPools()
+    selectionPools = db.child('selectionPools').get()
+    for building in selectionPools:
+        for gender in building.val():
+            if building.key() not in freshmenBuildings:
+                for roomType in gender:
+                    compareLists(roomType)
+                    #print("hi")
+            else:
+                compareLists(gender)
+    
+    return 0
+
+createPreferenceLists()
+
+
+
+
+
 
 #INITIALIZE SELECTION POOLS
 """buildingList = {'Delft House', 'Groningen House', 'Hague House', 'Leiden House', 'Rotterdam House',
@@ -62,15 +94,20 @@ def setSelectionPools():
                    'Quincy House', 'Suffolk Hall', 'Graduate'}"""
 
 """
-buildingList = ['Suffolk Hall']
+buildingList = ['Alliance Hall']
 for building in buildingList:
     genderPools = {"Male" : {
         "Single" : [""],
-        "Triple" : [""]
+        "Double" : [""],
+        "Triple" : [""],
+        "Quad" : [""]
     },
                    "Female" : {
         "Single" : [""],
-        "Triple" : [""]
+        "Double" : [""],
+        "Triple" : [""],
+        "Quad" : [""]
     }}
 
-    db.child('selectionPools').child(building).set(genderPools)"""
+    db.child('selectionPools').child(building).set(genderPools)
+"""
