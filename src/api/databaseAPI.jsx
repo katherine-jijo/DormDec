85ac0db4708023170a0f96c28610de419c4a8063
jkidding391 <134910549+jkidding391@users.noreply.github.com>
2024-04-26 20:38:06 -0400

    Fixed how newly registered user data is saved

--- src/api/databaseAPI.jsx
+++ src/api/databaseAPI.jsx
@@ -5,11 +5,15 @@ import { db } from "../firebaseConfig";
 // Function to save user data to the database
 export const saveUserData = async (localId, email, studentID) => {
   const userData = {
-    localId: localId,
-    email: email,
-    studentID: studentID
+    userData : {
+      localId: localId,
+      email: email,
+    },
+    userID: studentID
   };
 
   // Save userData under the user's localId in your database
   await set(ref(db, 'users/' + localId), userData);
+  await set(ref(db, 'localIdStorage/' + studentID.toString()), localId)
 };
+
