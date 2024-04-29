57e5ac68cb110b8d6ca36af4de7c3fc6abcf096f
DevenRam <deven@ramch.net>
2024-04-29 00:16:20 -0400

    Fixed window title, favicon, and made several QoL changes regarding buttons and resizing windows.

--- src/Components/LoginComponent.jsx
+++ src/Components/LoginComponent.jsx
@@ -1,77 +1,69 @@
-import React, { useState } from 'react';
-import { LoginAPI } from '../api/AuthAPI';
-import '../Sass/LoginComponent.scss';
+import React, { useState } from "react";
+import { LoginAPI } from "../api/AuthAPI";
+import "../Sass/LoginComponent.scss";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import DormDecidersLogo from "../assets/dormLogo.png";
 
 export default function LoginComponent() {
-  let navigate = useNavigate();
-  const [credentials, setCredentials] = useState({ email: '', password: '', studentID: '' });
+    let navigate = useNavigate();
+    const [credentials, setCredentials] = useState({ email: "", password: "", studentID: "" });
 
-  const login = async () => {
-    try {
-      let res = await LoginAPI(credentials.email, credentials.password, credentials.studentID);
-      toast.success("Signed In to Dorm Deciders!");
-      localStorage.setItem("userEmail", res.user.email);
-      navigate("/home");
-    } catch (err) {
-      console.log(err);
-      toast.error("Please Check your Credentials");
-    }
-  };
+    const login = async () => {
+        try {
+            let res = await LoginAPI(credentials.email, credentials.password, credentials.studentID);
+            toast.success("Signed In to Dorm Deciders!");
+            localStorage.setItem("userEmail", res.user.email);
+            navigate("/home");
+        } catch (err) {
+            console.log(err);
+            toast.error("Please Check your Credentials");
+        }
+    };
 
-  return (
-    <div className="login-wrapper">
-      <div className="login-wrapper-inner">
+    return (
+        <div className="login-wrapper">
+            <div className="login-wrapper-inner">
+                <img src={DormDecidersLogo} className="dormLogo" />
 
-      <img src={DormDecidersLogo} className="dormLogo" />
+                <div className="login-wrapper-inner">
+                    <h1 className="heading">Sign in</h1>
+                    <p className="sub-heading">Find your Perfect Match!</p>
 
-      <div className="login-wrapper-inner">
-        <h1 className="heading">Sign in</h1>
-        <p className="sub-heading">Find your Perfect Match!</p>
-
-        <div className="auth-inputs">
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentials, email: event.target.value })
-            }
-            type="email"
-            className="common-input"
-            placeholder="Email Address"
-          />
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentials, password: event.target.value })
-            }
-            type="password"
-            className="common-input"
-            placeholder="Password"
-          />
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentials, studentID: event.target.value })
-            }
-            type="text"
-            className="common-input"
-            placeholder="Hofstra Student ID"
-          />
+                    <div className="auth-inputs">
+                        <input
+                            onChange={(event) => setCredentials({ ...credentials, email: event.target.value })}
+                            type="email"
+                            className="common-input"
+                            placeholder="Email Address"
+                        />
+                        <input
+                            onChange={(event) => setCredentials({ ...credentials, password: event.target.value })}
+                            type="password"
+                            className="common-input"
+                            placeholder="Password"
+                        />
+                        <input
+                            onChange={(event) => setCredentials({ ...credentials, studentID: event.target.value })}
+                            type="text"
+                            className="common-input"
+                            placeholder="Hofstra Student ID"
+                        />
+                    </div>
+                    <button onClick={login} className="login-btn">
+                        Sign in
+                    </button>
+                </div>
+                <hr className="hr-text" data-content="or" />
+                <div className="google-btn-container">
+                    <p className="go-to-signup">
+                        New to Dorm Deciders?{" "}
+                        <span className="join-now" onClick={() => navigate("/register")}>
+                            Join now
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
-        <button onClick={login} className="login-btn">
-          Sign in
-        </button>
-      </div>
-      <hr className="hr-text" data-content="or" />
-      <div className="google-btn-container">
-        <p className="go-to-signup">
-          New to Dorm Deciders?{" "}
-          <span className="join-now" onClick={() => navigate("/register")}>
-            Join now
-          </span>
-        </p>
-      </div>
-    </div>
-    </div>
-
-  );
+    );
 }
